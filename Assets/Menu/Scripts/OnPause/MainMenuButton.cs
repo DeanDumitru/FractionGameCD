18abb1265591b1c459477976b1f014fd6f867483
DeanDumitru <dean_dumitru@yahoo.com>
2016-03-05 07:17:16 +0200

    Update
    Added menu when escape is pressed for both maps.
    Added sounds to the environment and character movement.
    Deleted old scene.
    Have all cogs with 2,3,4,5,6 spokes ready.

---
+++ Assets/Menu/Scripts/OnPause/MainMenuButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButton : MonoBehaviour {
+
+	public void click()
+    {
+        Application.LoadLevel("Menu");
+    }
+}
---
+++ Assets/Menu/Scripts/OnPause/OnPause.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnPause : MonoBehaviour {
+
+    public GameObject MenuToDisplay;
+    public GameObject Character;
+    public bool isPause = false;
+    
+
+	void Start ()
+    {
+        Cursor.visible = false;
+        //MenuToDisplay.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPause = !isPause;
+            if (isPause)
+            {
+                Time.timeScale = 0;
+                Cursor.visible = true;
+                MenuToDisplay.SetActive(true);
+                Character.SetActive(false);
+            }
+            else if (isPause == false)
+            {
+                Time.timeScale = 1;
+                Cursor.visible = false;
+                MenuToDisplay.SetActive(false);
+                Character.SetActive(true);
+            }
+        }
+    }
+}
