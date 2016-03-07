c01f54e1386f2eb70dbd776e409c1914f9cffd61
ChrisDrech92 <Drechslerc@uindy.edu>
2016-03-07 12:16:45 -0500

    Added MinionRunner

---
+++ Assets/MinionRunner/Scripts/IncreaseGravity.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncreaseGravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Physics.gravity = new Vector3(0, -55, 0);
+	}
+}
--- Assets/MinionRunner/Scripts/Player/Controller.cs
+++ Assets/MinionRunner/Scripts/Player/Controller.cs
@@ -6,33 +6,40 @@ public class Controller : MonoBehaviour
     public float speed = 6f;
     public float jump;
     public float jumpRate;
+    public float fireRate;
+    public float power;
+
     [HideInInspector]
     public static bool setActive;
 
     Vector3 movement;
     Animator anim;
     Rigidbody playerRigidbody;
+
     int floorMask;
     float camRayLength = 1000f;
 
+    public Rigidbody bullet;
+    public Transform shotSpawn;
+
     private float nextJump;
+    private float nextFire;
 
     void Awake()
     {
         floorMask = LayerMask.GetMask("Floor");
         anim = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody>();
+    //    shot = ball.GetComponent<Rigidbody>();
     }
 
 
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextJump)
-        {
-            nextJump = Time.time + jumpRate;
-            playerRigidbody.AddForce(transform.up * jump, ForceMode.Impulse);
-        }
+        Bullet();
 
+        Jumping();
+ 
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         // if (setActive == true)
@@ -48,7 +55,7 @@ public class Controller : MonoBehaviour
         movement = movement.normalized * speed * Time.deltaTime; // time between every call
         playerRigidbody.MovePosition(transform.localPosition + movement);
         //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
-    }
+    } 
 
     void OnTriggerEnter(Collider other)
     {
@@ -62,5 +69,29 @@ public class Controller : MonoBehaviour
     {
         bool walking = h != 0f || v != 0f;
         anim.SetBool("IsWalking", walking);
+    }
+
+    private void Jumping()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextJump)
+        {
+            nextJump = Time.time + jumpRate;
+            playerRigidbody.AddForce(transform.up * jump, ForceMode.Impulse);
+        }
+    }
+
+    public Vector3 angleShot = new Vector3(0, 0, 0);
+    public float angles;
+
+    private void Bullet()
+    {
+        if (Input.GetKeyDown(KeyCode.Y) && Time.time > nextFire)
+        {
+            Rigidbody clone;
+            nextFire = Time.time + fireRate;
+            clone = Instantiate(bullet, shotSpawn.position, shotSpawn.rotation) as Rigidbody;
+            //clone.velocity = transform.forward * power;
+            clone.GetComponent<Rigidbody>().AddForce(angleShot*power, ForceMode.Impulse);
+        }
     }
 }
