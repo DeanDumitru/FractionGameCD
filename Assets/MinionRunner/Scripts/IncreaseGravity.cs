23b2892177c4df471bddfcc22e7d6a8a4c88eec4
DeanDumitru <dean_dumitru@yahoo.com>
2016-04-13 14:27:54 -0400

    Fixed Gravity
    Update for gravity when the player moves from one level to another

--- Assets/MinionRunner/Scripts/IncreaseGravity.cs
+++ Assets/MinionRunner/Scripts/IncreaseGravity.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class IncreaseGravity : MonoBehaviour {
 
+	public float value;
 	// Use this for initialization
 	void Start () {
-        Physics.gravity = new Vector3(0, -55, 0);
+        Physics.gravity = new Vector3(0, value, 0);
 	}
 }
